e0a016bbc773adf44aa4cc3389c40fc2e1253575
acejarvis <acejarvis98@gmail.com>
2018-11-03 06:15:59 -0400

    updated system volume control

--- volumeControl/pyread.py
+++ volumeControl/pyread.py
@@ -1,36 +1,74 @@
 #Jarvis Wang
-#test on Mac using port /dev/cu.usbmoderm145401
-import applescript
+#test on Mac using port /dev/cu.usbmodem145401
+from osax import *
 import serial
-import math
-buttonStateFlag = True
-playStateFlag = False
+import time
+#import pyautogui
+
+
 #read data from arduino
-ser = serial.Serial("/dev/cu.usbmoderm145401", 9600, timeout = 0.5) #serial ports setting on Mac
+#ser = serial.Serial("/dev/cu.usbmodem145401", 9600) #serial ports setting on Mac
 #ser = serial.Serial("COM3", 9600, timeout = 0.5) #serial ports setting on Windows
 
-while True:
-    data = ser.readline()
-    #Self-locking Push Switch for Play/Pause 
-    if data == 400 and buttonStateFlag == True: 
-        playStateFlag = not playStateFlag
-        if playStateFlag:
-            print("Play") #need api calling
-        else:
-            print("Pause") #need api calling
-        buttonStateFlag = False
-    
-    elif data < 400:
-        buttonStateFlag = True
-        #volume control
-        if data < 105:
-            volume = data
-            #print("Volume: %d" %volume)
+ser = 0
 
-        #play previous
-        elif data == 200:
-            print("Play Previous") #need api calling
-        #play next
-        elif data == 300:
-            print("Play Next") #need api calling
-    applescript.Applescript("set volume output volume %d" % volume).run() #System volume control on Mac
+#Function to Initialize the Serial Port
+def init_serial():
+    global ser          #Must be declared in Each Function
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    ser.port = '/dev/cu.usbmodem145301'   #COM Port Name Start from 0
+    #ser.port = '/dev/ttyUSB0' #If Using Linux
+
+    #Specify the TimeOut in seconds, so that SerialPort
+    #Doesn't hangs
+    ser.timeout = 10
+    ser.open()          #Opens SerialPort
+
+    # print port open or closed
+   
+#Function Ends Here
+
+init_serial()
+
+vol = OSAX()
+
+while True:
+    incoming = ser.readline()
+    
+    print (int(incoming.decode('utf-8'))) # print the current volume
+    volume = float(int(incoming.decode('utf-8'))/103*7) # convert the percentage to osax volume scale
+    vol.set_volume(volume) #system volume control
+
+
+
+
+
+# buttonStateFlag = True
+# playStateFlag = False
+
+# while True:
+#     data = ser.readline()
+#     #Self-locking Push Switch for Play/Pause 
+#     if data == 400 and buttonStateFlag == True: 
+#         playStateFlag = not playStateFlag
+#         if playStateFlag:
+#             print("Play") #need api calling
+#         else:
+#             print("Pause") #need api calling
+#         buttonStateFlag = False
+    
+#     elif data < 400:
+#         buttonStateFlag = True
+#         #volume control
+#         if data < 105:
+#             volume = data
+#             #print("Volume: %d" %volume)
+
+#         #play previous
+#         elif data == 200:
+#             print("Play Previous") #need api calling
+#         #play next
+#         elif data == 300:
+#             print("Play Next") #need api calling
+#     applescript.Applescript("set volume output volume %d" % volume).run() #System volume control on Mac
